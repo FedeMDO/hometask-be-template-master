ef9b6a19b1c5c4c1e54316cce171bb2e2d66f579
Federico Montes <federicomdo97@gmail.com>
2023-07-23 23:07:08 -0300

    making balances service more testable

--- src/services/balances.service.js
+++ src/services/balances.service.js
@@ -1,55 +1,66 @@
 const { sequelize, Profile, Contract, Job } = require('../models');
 const { Op } = require('sequelize');
 
-async function depositMoney(userId, depositAmount) {
-  return await sequelize.transaction(async (t) => {
-    const withTransactionAndLock = {
-      transaction: t,
-      lock: true,
-    };
-
-    // sum job debt ('jobs to pay')
-    const jobsDebt = await Job.findOne({
-      include: {
-        model: Contract,
-        where: {
-          ClientId: userId,
-          status: {
-            [Op.ne]: 'terminated', // only 'new' and 'in_progress' contracts
-          },
+async function getUserDebt(userId, transaction) {
+  // sum job debt ('jobs to pay')
+  const jobsDebt = await Job.findOne({
+    include: {
+      model: Contract,
+      where: {
+        ClientId: userId,
+        status: {
+          [Op.ne]: 'terminated', // only 'new' and 'in_progress' contracts
         },
       },
-      attributes: [[sequelize.fn('sum', sequelize.col('price')), 'total_debt']],
-      where: {
-        paid: null,
-      },
-      ...withTransactionAndLock,
-    });
+    },
+    attributes: [[sequelize.fn('sum', sequelize.col('price')), 'total_debt']],
+    where: {
+      paid: null,
+    },
+    transaction,
+    lock: true,
+  });
 
-    if (!jobsDebt) {
-      throw new Error('no jobs to pay');
-    }
+  if (!jobsDebt) {
+    throw new Error('no jobs to pay');
+  }
 
-    const debt = jobsDebt.toJSON();
+  const debt = jobsDebt.toJSON();
 
-    // a client can't deposit more than 25% his total of 'jobs to pay'
-    // round up to the nearest integer
-    const maximumAmount = Math.ceil(debt.total_debt * 0.25);
+  return debt;
+}
 
-    if (depositAmount > maximumAmount) {
-      // rollback is managed by sequelize
-      throw new Error(`depositAmount must be less or equal than ${maximumAmount}`);
-    }
+async function incrementProfileBalance(userId, amount, transaction) {
+  return await Profile.increment('balance', {
+    by: amount,
+    where: { id: userId },
+    transaction,
+    lock: true,
+  });
+}
 
-    // increment client's balance
-    await Profile.increment('balance', {
-      by: depositAmount,
-      where: { id: userId },
-      ...withTransactionAndLock,
-    });
+function checkValidDepositAmount(totalDebt, depositAmount) {
+  // a client can't deposit more than 25% his total of 'jobs to pay'
+  // round up to the nearest integer
+  const maximumAmount = Math.ceil(totalDebt * 0.25);
+
+  if (depositAmount > maximumAmount) {
+    // rollback is managed by sequelize
+    throw new Error(`depositAmount must be less or equal than ${maximumAmount}`);
+  }
+}
+
+async function depositMoney(userId, depositAmount) {
+  return await sequelize.transaction(async (t) => {
+    const debt = await getUserDebt(userId, t);
+    checkValidDepositAmount(debt.total_debt, depositAmount);
+    await incrementProfileBalance(userId, depositAmount, t);
   });
 }
 
 module.exports = {
   depositMoney,
+  getUserDebt,
+  incrementProfileBalance,
+  checkValidDepositAmount,
 };
